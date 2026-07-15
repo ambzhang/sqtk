ab824265ef6f5208ddfbe3bdc1656fad508d5d4a
ambzhang <450145249@qq.com>
2026-07-15 10:53:28 +0800

    feat: deploy web client at /remote-control
    - Move landing page to /remote-control/about
    - Add web client HTML to /remote-control via Next.js rewrite
    - Users can now visit sqtk.site/remote-control to connect to phones
    - Keep APK download link on about page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/remote-control',
+        destination: '/remote-control.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
